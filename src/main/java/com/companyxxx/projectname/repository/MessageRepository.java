86dd009333e717fd7203d894730dffec62c45e38
luzj <1414377646@qq.com>
2019-01-25 17:20:18 +0800

    添加@Repository

--- src/main/java/com/companyxxx/projectname/repository/MessageRepository.java
+++ src/main/java/com/companyxxx/projectname/repository/MessageRepository.java
@@ -2,11 +2,13 @@ package com.companyxxx.projectname.repository;
 
 import com.companyxxx.projectname.domain.s.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author: luzj
  * @date: 2019-01-24
  * @description:
  */
+@Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
 }
--- src/main/java/com/companyxxx/projectname/repository/UserRepository.java
+++ src/main/java/com/companyxxx/projectname/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.companyxxx.projectname.repository;
 
 import com.companyxxx.projectname.domain.p.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author: luzj
  * @date: 2019-01-24
  * @description: respository 样例展示
  */
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }

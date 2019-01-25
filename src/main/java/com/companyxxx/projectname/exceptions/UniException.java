e4b702b4a75ff0b22d22a2095b1d438452277d94
luzj <1414377646@qq.com>
2019-01-25 14:54:42 +0800

    自定义异常

---
+++ src/main/java/com/companyxxx/projectname/exceptions/UniException.java
@@ -0,0 +1,13 @@
+package com.companyxxx.projectname.exceptions;
+
+/**
+ * @author: luzj
+ * @date: 2019-01-25
+ * @description: 自定义异常，示例拦截GlobalException
+ */
+public class UniException extends Exception {
+
+    public UniException(String message) {
+        super(message);
+    }
+}
